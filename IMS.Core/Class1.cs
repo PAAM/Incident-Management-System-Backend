d796ab8b8ef81d8988ad88109f56f52cc8b32b3e
Pedro Aguirre <pedroa.aguirrem@hotmail.com>
2026-07-12 17:10:59 -0500

    chore: initialize solution structure (IMS-001)
    Description: I created the projects to use in this application.

---
+++ IMS.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace IMS.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ IMS.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace IMS.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
